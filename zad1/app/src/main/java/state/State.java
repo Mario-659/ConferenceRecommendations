f1e5806968921f4aaa46498fec754d62ee88b4fb
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2021-11-27 23:16:50 +0100

    works for even number of participants

--- zad1/app/src/main/java/state/State.java
+++ zad1/app/src/main/java/state/State.java
@@ -1,16 +1,18 @@
 package state;
 
+import algorithm.crossover.RankSelection;
+import algorithm.mutation.RandomMutation;
 import data.Person;
 import tools.Rand;
 
 import java.util.ArrayList;
 import java.util.List;
 
-//TODO not actually a DNA, a DNA is all connections of a person, change name of this class
-
+//TODO add extracting best genomes
 public class State {
     private static final int NUM_OF_CONNECTIONS = 5;
 
+    List<Population> populations;
     List<Genome> bestGenomes;
     int score;
 
@@ -20,11 +22,23 @@ public class State {
         this.score = value;
     }
 
+    private void setBestGenomes(){
+        List<Genome> genomes = new ArrayList<>();
+        for (Population population : populations) {
+            genomes.add(population.getBestGenome());
+        }
+        bestGenomes = genomes;
+    }
+
     public State(List<Person> persons, int numOfConnections){
         bestGenomes = new ArrayList<>();
         for (Person person : persons) {
             List<Person> connections = Rand.makeRandConnections(persons, person, numOfConnections);
             bestGenomes.add(new Genome(person, connections));
+        }
+        populations = new ArrayList<>();
+        for (Genome genome : bestGenomes) {
+            populations.add(new Population(genome, new RandomMutation(persons), new RankSelection()));
         }
         evaluate();
     }
@@ -35,13 +49,19 @@ public class State {
         StringBuilder string = new StringBuilder();
         string.append("Value of configuration: ");
         string.append(score);
-        for (Genome genome :
-                bestGenomes) {
+        for (Genome genome : bestGenomes) {
             string.append("\n");
             string.append(genome.toString());
         }
         return string.toString();
     }
-
     public int getScore(){return score;}
+
+    public void update(){
+        for (Population population : populations) {
+            population.nextGeneration();
+        }
+        setBestGenomes();
+        evaluate();
+    }
 }
